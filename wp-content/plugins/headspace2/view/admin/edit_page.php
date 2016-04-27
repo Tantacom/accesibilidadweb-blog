e4d2f6c14d1ed8088f89e82932f6cfa71f0f3035
Israel Martín García <israel.martin@tantacom.com>
2016-04-27 13:30:37 +0200

    Elimina plugins que estaban desactivados

--- wp-content/plugins/headspace2/view/admin/edit_page.php
+++
@@ -1,2 +0,0 @@
-<p id="jaxtag"><input type="text" name="tags_input" class="tags-input" id="tags-input" size="40" tabindex="3" value="<?php echo get_tags_to_edit( $post_ID ); ?>" /></p>
-<div id="tagchecklist"></div>
--- wp-content/plugins/headspace2/view/admin/head_admin.php
+++
@@ -1,6 +0,0 @@
-<?php if (!defined ('ABSPATH')) die ('No direct access allowed'); ?>
-<script type="text/javascript" charset="utf-8">
-	var wp_hs_base     = '<?php echo $this->url (); ?>/ajax.php';
-	var headspace_delete = '<?php echo $this->url () ?>/images/delete.png';
-	var wp_hs_mergetag = '<?php _e ('Are you sure you want to merge that tag?', 'headspace'); ?>';
-</script>
